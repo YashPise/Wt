73f711f324700371cf95823a681ff86ad1903c6f
YashPise <99723328+YashPise@users.noreply.github.com>
2022-11-16 22:48:18 +0530

    Assignments

---
+++ app.js
@@ -0,0 +1,20 @@
+// import express from 'express';
+// var express=require('express')
+// var app = express();
+
+// app.get('/',function(req,res){
+//     res.send("Hello World");
+// });
+
+// app.listen(8080,function(){
+//     console.log("Application listening to port 8080");
+// })
+
+var express=require("express")
+var app =express()
+app.get('/', function(req, res){
+    res.send("FUKKKKK you........")
+})
+app.listen(8080, function(){
+    console.log("Hey brooooooooooo")
+})
